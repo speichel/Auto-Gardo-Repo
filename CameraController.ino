8e9d5a26c1854e1251d8d901a93e84cc7f81bb4a
Steve Peichel <speichel@ceg-engineers.com>
2015-02-23 18:19:59 -0600

    Finished Clean up of file.

--- CameraController.ino
+++ CameraController.ino
@@ -13,12 +13,12 @@ const int rightPin = 2;
 const int upPin = 8;
 const int downPin = 12;
 
-const int led1Pin = 6;              // indicator
-const int led2Pin = 5;              // indicator
-const int led3Pin = 10;             // indicator
-const int led4Pin = 11;             // indicator
+const int led1Pin = 6;      // indicator
+const int led2Pin = 5;      // indicator
+const int led3Pin = 10;     // indicator
+const int led4Pin = 11;     // indicator
 
-const int outputPin = 9;            // pwm function will be disabled on pin 9 and 10 if using servo
+const int outputPin = 9;    // pwm function will be disabled on pin 9 and 10 if using servo
 const int outputPin2 = 3; 
 
 int leftPressed = 0;
@@ -26,14 +26,13 @@ int rightPressed = 0;
 int upPressed = 0;
 int downPressed = 0;
 
-void setup() 
-	{
-	myservo.attach(outputPin);         // attaches the servo on pin 9 to the servo object 
-	myservo2.attach(outputPin2);       // attaches the servo on pin 3 to the servo object 
+void setup() { 
+	myservo.attach(outputPin);          // attaches the servo on pin 9 to the servo object 
+	myservo2.attach(outputPin2);        // attaches the servo on pin 3 to the servo object 
 
 	pinMode(leftPin, INPUT);
-	pinMode(rightPin,INPUT);
-	pinMode(upPin,   INPUT);
+	pinMode(rightPin, INPUT);
+	pinMode(upPin, INPUT);
 	pinMode(downPin, INPUT);
 
 	pinMode(led1Pin, OUTPUT);
@@ -41,83 +40,52 @@ void setup()
 	pinMode(led3Pin, OUTPUT);
 	pinMode(led4Pin, OUTPUT);
 	} 
- 
-void loop() 
-	{ 
+
+void loop() {
 	leftPressed = digitalRead(leftPin);
 	rightPressed = digitalRead(rightPin);
 	upPressed = digitalRead(upPin);
 	downPressed = digitalRead(downPin);
 	 
-	if(leftPressed)
-	{
-	if(pos < maxDeg) pos += 20;
+	if(leftPressed){
+		if(pos < maxDeg) pos += 20; 
+		myservo.write(pos);                 // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led1Pin,HIGH);
+		delay(5);
+		myservo.write(90);
+		pos -= 20;
+		} else
+		digitalWrite(led1Pin,LOW);
+ 
+	if(rightPressed){
+		if(pos > minDeg) pos -= 20;
+		myservo.write(pos);              // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led2Pin,HIGH);
+		delay(5);
+		myservo.write(90);
+		pos += 20;
+		} else
+		digitalWrite(led2Pin,LOW);
+		delay(15);                       // waits 15ms for the servo to reach the position 
 
-	myservo.write(pos);                // tell servo to go to position in variable ‘pos’ 
-	digitalWrite(led1Pin,HIGH);
-	delay(5);
-	myservo.write(90);
-	pos -= 20;
-	}
-	else
-digitalWrite(led1Pin,LOW);
+	if(upPressed){
+		if(pos < maxDeg) pos += 20;
+		myservo2.write(pos);              // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led3Pin,HIGH);
+		delay(5);
+		myservo2.write(90);
+		pos -= 20;
+		} else
+		digitalWrite(led3Pin,LOW);
  
- 
- 
- 
- 
- 
- 
- if(rightPressed){
- if(pos > minDeg) pos -= 20;
-myservo.write(pos);              // tell servo to go to position in variable ‘pos’ 
-digitalWrite(led2Pin,HIGH);
-delay(5);
-myservo.write(90);
-pos += 20;
- }
- else
-digitalWrite(led2Pin,LOW);
- 
-delay(15);                       // waits 15ms for the servo to reach the position 
-
-
-
-
-
- if(upPressed){
- if(pos < maxDeg) pos += 20;
-
-myservo2.write(pos);              // tell servo to go to position in variable ‘pos’ 
-digitalWrite(led3Pin,HIGH);
-delay(5);
-myservo2.write(90);
-pos -= 20;
- }
- else
-digitalWrite(led3Pin,LOW);
- 
- 
- 
- 
- 
- 
- 
- if(downPressed){
- if(pos > minDeg) pos -= 20;
-myservo2.write(pos);              // tell servo to go to position in variable ‘pos’ 
-digitalWrite(led4Pin,HIGH);
-delay(5);
-myservo2.write(90);
-pos += 20;
- }
- else
-digitalWrite(led4Pin,LOW);
- 
-delay(15);                       // waits 15ms for the servo to reach the position 
-
-
-
-
-
-}
+	if(downPressed){
+		if(pos > minDeg) pos -= 20;
+		myservo2.write(pos);              // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led4Pin,HIGH);
+		delay(5);
+		myservo2.write(90);
+		pos += 20;
+		} else
+		digitalWrite(led4Pin,LOW);
+		delay(15);                       // waits 15ms for the servo to reach the position 
+	}	
