9ec5ce39b8bcfd04b25e0208f54c1cb2851fd4d6
Steve Peichel <speichel@ceg-engineers.com>
2015-02-23 19:07:18 -0600

    Finished Clean up of file.

--- Sabertooth.ino
+++ Sabertooth.ino
@@ -1,6 +1,3 @@
-//2/20/2015 Organizing this file before migrating to master file
-
-
 /***************************************************************************/
 //	Function: Measure the distance to obstacles in front and print the distance
 //			  value to the serial terminal.The measured distance is from 
@@ -45,6 +42,7 @@ SabertoothSimplified ST; // We'll name the Sabertooth object ST.
                          // If you want to use a pin other than TX->1, see the SoftwareSerial example
 const int kpl = -12;                         // Proportional control constants
 const int kpr = -12;
+
 int i = 0;
 int distance_setting_turning=16;
 int sonar_turning;
@@ -55,56 +53,41 @@ int a = 1;
 int b = 0;
 int c = 0;
 
+class Ultrasonic {
+	public:
+	  Ultrasonic(int pin);
+	  void DistanceMeasure(void);
+	  long microsecondsToCentimeters(void);
+	private:
+	  int _pin;//pin number of Arduino that is connected with SIG pin of Ultrasonic Ranger.
+	  long duration;// the Pulse time received;
+	};
+
+Ultrasonic::Ultrasonic(int pin) { _pin = pin; }
 
 
-
-
-class Ultrasonic
-{
-public:
-  Ultrasonic(int pin);
-  void DistanceMeasure(void);
-  long microsecondsToCentimeters(void);
-private:
-  int _pin;//pin number of Arduino that is connected with SIG pin of Ultrasonic Ranger.
-  long duration;// the Pulse time received;
-};
-
-
-Ultrasonic::Ultrasonic(int pin)
-{
-  _pin = pin;
-}
 /*Begin the detection and get the pulse back signal*/
 void Ultrasonic::DistanceMeasure(void)
-{
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(_pin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(_pin,LOW);
-  pinMode(_pin,INPUT);
-  duration = pulseIn(_pin,HIGH);
-}
+	{
+	pinMode(_pin, OUTPUT);
+	digitalWrite(_pin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(_pin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(_pin,LOW);
+	pinMode(_pin,INPUT);
+	duration = pulseIn(_pin,HIGH);
+	}
+
 /*The measured distance from the range 0 to 400 Centimeters*/
 long Ultrasonic::microsecondsToCentimeters(void)
-{
-  return duration/29/2;	
-}
+	{
+	  return duration/29/2;	
+	}
 
-
-
-
-
-
-
-
-
+/*minimum movement in forward direction:    ST.drive(20);    delay(20);    ST.drive(0);    delay(800);*/
 void maneuver(int distance_setting_turning, int calc_sonar_turning, int distance_setting_front, int sonar_front, int msTime, int a)
-{
-        //minimum movement in forward direction:    ST.drive(20);    delay(20);    ST.drive(0);    delay(800);
-      
+	{
         ST.drive(40*a);
         delay(600);
     
@@ -115,113 +98,88 @@ void maneuver(int distance_setting_turning, int calc_sonar_turning, int distance
         delay(100);
      
     
-        if(msTime==-1)                                   // if msTime = -1
-        {                                  
-          ST.turn(0);                            // Stop servo signals
-          ST.drive(0);   
-        }
-        delay(msTime);                                   // Delay for msTime
+        if(msTime==-1)     // if msTime = -1
+		{                                  
+		  ST.turn(0);      // Stop servo signals
+		  ST.drive(0);   
+		}
+
+        delay(msTime);     // Delay for msTime
       
-          if (i >110)
-        {
-          Serial.println("Inside Maneuver: ");
-          Serial.println("**Leaving Maneuver***");
-      
-          i=0;
-        } 
-        else 
-        {
-          i=i+1;
-        }
-
-}
-
-
-
-
-
-
-Ultrasonic ultrasonic7(7);
-Ultrasonic ultrasonic5(5);
-
+        if (i >110)
+		{
+		  Serial.println("Inside Maneuver: ");
+		  Serial.println("**Leaving Maneuver***");
+		  i=0;
+		} else
+		i=i+1;
 
 void setup()
-{
-  SabertoothTXPinSerial.begin(9600); // This is the baud rate you chose with the DIP switches.             
-  ST.drive(0); // The Sabertooth won't act on mixed mode until
-  ST.turn(0);  // it has received power levels for BOTH throttle and turning, since it
-               // mixes the two together to get diff-drive power levels for both motors.
-               // So, we set both to zero initially.
-               
-               // Mixed mode tips:
-//   drive() should go forward and back, turn() should go right and left.
-//     If this is reversed, swap M2A and M2B.
-//   Positive on drive() should go forward, negative should go backward.
-//     If this is reversed, swap A and B on both M1 and M2.
-//   Positive on turn() should go right, negative should go left.
-//     If this is reversed, swap M1 and M2.
-
-// In this sample, the SLOW sweep (left-to-right) here is turning,
-// and the FAST sweep (backwards-to-forwards) is throttle.
-
-}
+	{
+	SabertoothTXPinSerial.begin(9600); // This is the baud rate you chose with the DIP switches.             
+	ST.drive(0);                       // The Sabertooth won't act on mixed mode until
+	ST.turn(0);                        // it has received power levels for BOTH throttle and turning, since it
+		                           // So, we set both to zero initially.
+        	                           // Mixed mode tips:
+		                           //   drive() should go forward and back, turn() should go right and left.
+		                           //     If this is reversed, swap M2A and M2B.
+		                           //   Positive on drive() should go forward, negative should go backward.
+		                           //     If this is reversed, swap A and B on both M1 and M2.
+		                           //   Positive on turn() should go right, negative should go left.
+		                           //     If this is reversed, swap M1 and M2.
+	                                   // In this sample, the SLOW sweep (left-to-right) here is turning,
+	                                   // and the FAST sweep (backwards-to-forwards) is throttle.
+	}
 
 void loop()
-{
+	{
+	long RangeInCentimeters7;
+	long RangeInCentimeters5;
+	float distance7;
+	float distance5;
 
+	ultrasonic7.DistanceMeasure();// get the current signal time;
+	ultrasonic5.DistanceMeasure();// get the current signal time;
 
-  long RangeInCentimeters7;
-  long RangeInCentimeters5;
-  float distance7;
-  float distance5;
+	RangeInCentimeters7 = ultrasonic7.microsecondsToCentimeters();//convert the time to centimeters
+	RangeInCentimeters5 = ultrasonic5.microsecondsToCentimeters();//convert the time to centimeters
 
+							//Serial.println("The distance to obstacles in front is: ");
+	distance7 = RangeInCentimeters7/2.54;
+	distance5 = RangeInCentimeters5/2.54;
+							//Serial.print(distance);//0~400cm and 0~157 inches
+							//Serial.println(" inches-decimal");
+	delay(10);
 
-  ultrasonic7.DistanceMeasure();// get the current signal time;
-  ultrasonic5.DistanceMeasure();// get the current signal time;
-  RangeInCentimeters7 = ultrasonic7.microsecondsToCentimeters();//convert the time to centimeters
-  RangeInCentimeters5 = ultrasonic5.microsecondsToCentimeters();//convert the time to centimeters
-  //Serial.println("The distance to obstacles in front is: ");
-  distance7 = RangeInCentimeters7/2.54;
-  distance5 = RangeInCentimeters5/2.54;
-  //Serial.print(distance);//0~400cm and 0~157 inches
-  //Serial.println(" inches-decimal");
-  delay(10);
+							// Left and right proportional control calculations
+	sonar_turning = distance7;
+	sonar_front = distance5;
+	
+	distance_setting_turning = 12;
+	distance_setting_front = 18;  
+	
+	int calc_sonar_turning = (distance_setting_turning - sonar_turning) * kpr;
 
+	if (sonar_front >= 24 && sonar_front <= 12)  
+		{
+		  a = a;
+		} else if (sonar_front > 24) 
+		{
+		  a = 1;
+		} else if (sonar_front<12)
+		{
+		  a = -1;
+		}
 
-  // Left and right proportional control calculations
+	maneuver(distance_setting_turning, calc_sonar_turning, distance_setting_front, sonar_front, 20, a);       // Drive levels set speeds
 
-  sonar_turning = distance7;
-  sonar_front = distance5;
-  
-  distance_setting_turning = 12;
-  distance_setting_front = 18;  
-  
-  int calc_sonar_turning = (distance_setting_turning - sonar_turning) * kpr;
-
-  if (sonar_front >= 24 && sonar_front <= 12)  
-  {
-    a = a;
-  } else if (sonar_front > 24) 
-  {
-    a = 1;
-  } else if (sonar_front<12)
-  {
-    a = -1;
-  }
-
-  maneuver(distance_setting_turning, calc_sonar_turning, distance_setting_front, sonar_front, 20, a);       // Drive levels set speeds
-
-  if ( i > 110 )
-  {
-    Serial.println("Inside Main Program: ");
-    Serial.println("***Leaving Main Program*** ");
-  } 
-  else
-  {
-    //nothing
-  }
-
-}
-
-
-
+	if ( i > 110 )
+		{
+		  Serial.println("Inside Main Program: ");
+		  Serial.println("***Leaving Main Program*** ");
+		} 
+		else
+		{
+		//nothing
+		}
+	}
