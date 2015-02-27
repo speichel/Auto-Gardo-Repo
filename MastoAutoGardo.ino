ae0b1eec76ea9e2203ff9fd0e209b9524ca04dc2
Steve Peichel <speichel@ceg-engineers.com>
2015-02-26 18:10:14 -0600

    Finished adding the Camo section, still need to test in A-IDE.

--- MastoAutoGardo.ino
+++ MastoAutoGardo.ino
@@ -25,9 +25,12 @@
 #include <Servo.h>                           // Include servo library
 #include <SabertoothSimplified.h>
 
+Servo myservo;           //Initialize servo 
+Servo myservo2;          //Initialize servo2  
+
 SabertoothSimplified ST; // We'll name the Sabertooth object ST.
                          // For how to configure the Sabertooth, see the DIP Switch Wizard for
-                         //   http://www.dimensionengineering.com/datasheets/SabertoothDIPWizard/start.htm
+                         // http://www.dimensionengineering.com/datasheets/SabertoothDIPWizard/start.htm
                          // Be sure to select Simplified Serial Mode for use with this library.
                          // This sample uses a baud rate of 9600.
                          //
@@ -40,6 +43,31 @@ SabertoothSimplified ST; // We'll name the Sabertooth object ST.
 const int kpl = -12;                         // Proportional control constants
 const int kpr = -12;
 
+//Variables for Camo 
+int pos = 90;    // variable to store the servo position 
+const int maxDeg = 160;
+const int minDeg = 5;
+
+const int leftPin = 4;
+const int rightPin = 2;
+const int upPin = 8;
+const int downPin = 12;
+
+const int led1Pin = 6;      // indicator
+const int led2Pin = 5;      // indicator
+const int led3Pin = 10;     // indicator
+const int led4Pin = 11;     // indicator
+
+const int outputPin = 9;    // pwm function will be disabled on pin 9 and 10 if using servo
+const int outputPin2 = 3; 
+
+int leftPressed = 0;
+int rightPressed = 0;
+int upPressed = 0;
+int downPressed = 0;
+
+
+//Variables here for GartoCarto
 int i = 0;
 int distance_setting_turning=16;
 int sonar_turning;
@@ -50,6 +78,8 @@ int a = 1;
 int b = 0;
 int c = 0;
 
+
+//Variables here for AutoGardo
 int sensorPin = 0;      //the analog pin the TMP36's Vout (sense) pin is connected to
 			//the resolution is 10 mV / degree centigrade with a
 			//500 mV offset to allow for negative temperatures
@@ -163,6 +193,19 @@ void setup() {
 		                           //     If this is reversed, swap M1 and M2.
 	                                   // In this sample, the SLOW sweep (left-to-right) here is turning,
 	                                   // and the FAST sweep (backwards-to-forwards) is throttle.
+	//Camo Setup
+	myservo.attach(outputPin);          // attaches the servo on pin 9 to the servo object 
+	myservo2.attach(outputPin2);        // attaches the servo on pin 3 to the servo object 
+
+	pinMode(leftPin, INPUT);
+	pinMode(rightPin, INPUT);
+	pinMode(upPin, INPUT);
+	pinMode(downPin, INPUT);
+
+	pinMode(led1Pin, OUTPUT);
+	pinMode(led2Pin, OUTPUT);
+	pinMode(led3Pin, OUTPUT);
+	pinMode(led4Pin, OUTPUT);
 	}
 
 void loop() {
@@ -348,7 +391,55 @@ void loop() {
 		{
 		//nothing
 		}
-	}
+
+	//Camo void loop section
+	leftPressed = digitalRead(leftPin);
+	rightPressed = digitalRead(rightPin);
+	upPressed = digitalRead(upPin);
+	downPressed = digitalRead(downPin);
+	 
+	if(leftPressed){
+		if(pos < maxDeg) pos += 20; 
+		myservo.write(pos);                 // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led1Pin,HIGH);
+		delay(5);
+		myservo.write(90);
+		pos -= 20;
+		} else
+		digitalWrite(led1Pin,LOW);
+ 
+	if(rightPressed){
+		if(pos > minDeg) pos -= 20;
+		myservo.write(pos);              // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led2Pin,HIGH);
+		delay(5);
+		myservo.write(90);
+		pos += 20;
+		} else
+		digitalWrite(led2Pin,LOW);
+		delay(15);                       // waits 15ms for the servo to reach the position 
+
+	if(upPressed){
+		if(pos < maxDeg) pos += 20;
+		myservo2.write(pos);              // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led3Pin,HIGH);
+		delay(5);
+		myservo2.write(90);
+		pos -= 20;
+		} else
+		digitalWrite(led3Pin,LOW);
+ 
+	if(downPressed){
+		if(pos > minDeg) pos -= 20;
+		myservo2.write(pos);              // tell servo to go to position in variable ‘pos’ 
+		digitalWrite(led4Pin,HIGH);
+		delay(5);
+		myservo2.write(90);
+		pos += 20;
+		} else
+		digitalWrite(led4Pin,LOW);
+		delay(15);                       // waits 15ms for the servo to reach the position 
+	}	
 
 float getTemperature(){
 	SHT_sendCommand(B00000011, SHT_dataPin, SHT_clockPin);  //Return Temperature in Celsius
